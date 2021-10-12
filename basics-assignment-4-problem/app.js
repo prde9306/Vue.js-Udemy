b8b1b552b65eea4dd9266756c9bc1fec138d06f5
prde9306 <prde9306@gmail.com>
2021-10-12 22:24:01 +0900

    section3

--- basics-assignment-4-problem/app.js
+++ basics-assignment-4-problem/app.js
@@ -2,11 +2,26 @@ const app = Vue.createApp({
 
   data(){
     return{
-
+      inputClass : '',
+      paragraphIsVisible: true,
+      inputBackgroundColor:''
     }
   }
   ,methods:{
+    toggleParagraphVisible(){
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    }
+  },
+  computed:{
+    paraClasses(){
+      return {
+        user1: this.inputClass ==='user1',
+        user2: this.inputClass ==='user2',
+        visible: this.paragraphIsVisible,
+        hidden: !this.paragraphIsVisible
 
+      }
+    }
   }
 
 });
